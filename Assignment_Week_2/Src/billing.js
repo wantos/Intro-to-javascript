35c01978abf17e7502cd1b2602826f0bda91cd24
Wantono-Emmanuel <emmasirabo78@gmail.com>
2019-06-13 10:45:47 +0300

    first commit

---
+++ Assignment_Week_2/Src/billing.js
@@ -0,0 +1,7 @@
+
+/*This is the percentage that determines the transaction charge*/
+const transactionCharge = 2.5%;
+var amountTendered = 100000;
+ console.log(This is What the billed amount is(less transaction charges));
+ console.log(amountTendered-(amountTendered * transactionCharge));
+ 
