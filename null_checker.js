71901b176c1c7a37b9fe3fddaf8bd38f01f18697
Wantono Emmanuel <51445540+wantos@users.noreply.github.com>
2019-06-14 09:29:11 +0300

    Add files via upload

---
+++ null_checker.js
@@ -0,0 +1,12 @@
+for (count = 10; 
+	count <= 10; count++)
+{
+ var numberCheck = prompt("What is your item number please!")
+if (numberCheck > 0) {
+	console.log(1);}
+if (numberCheck < -0){
+	console.log(-1)
+}
+if (numberCheck == 0) {
+	console.log(0)
+}}
